9f0811ed23aea04e302a39b7c04e27add0c5a9d1
Pablo Pirata <pablo@studocu.com>
2025-05-18 19:50:31 +0200

    add files

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'build',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   eslint: {
     ignoreDuringBuilds: true,
   },
